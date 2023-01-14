5d6f2038934191ef172e39eb3459d06aaa093f09
moein9gh <moein9gh@gmail.com>
2023-01-14 14:23:10 +0330

    test husky

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
 
         gateway.Websocket.NewServerOnSamePort(gateway.HttpServer.GetServer()!);
 
-        gateway.GrpcServer.NewServer();
+        gateway.GrpcServer.NewServer()
 
     } catch (e) {
     }
